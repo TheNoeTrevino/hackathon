083c441512e455688988cfcaf1b81c3400f8c6a9
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-26 15:33:19 -0500

    add: riddles

--- frontend/src/pages/riddle-game/RiddleGame.tsx
+++ frontend/src/pages/riddle-game/RiddleGame.tsx
@@ -1,50 +1,71 @@
-import { Box, Button, Typography } from "@mui/material";
-import Riddles from "./Riddles";
+import { Box, ButtonBase, Typography } from "@mui/material";
+import riddleRepo from "./Riddles";
 
 const RiddleGame = () => {
+  const randomIndex = Math.floor(Math.random() * riddleRepo.length);
+  const riddle = riddleRepo[randomIndex];
   const checkAnswer = (answer: string) => {
-    if (answer === Riddles.answer) {
+    if (answer === riddle.answer) {
       alert("Correct!");
     } else {
       alert("Incorrect!");
     }
   };
+
+  const riddleRows = [
+    riddle.answerChoices.slice(0, 2),
+    riddle.answerChoices.slice(2, 4),
+  ];
+  const answerChoiceStyles = {
+    px: 2,
+    py: 1,
+    backgroundColor: "primary.main",
+    color: "white",
+    borderRadius: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  };
+
   return (
     <>
       <Box
         sx={{
           mt: 20,
-          ml: 2,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           alignSelf: "center",
         }}
       >
-        <Typography variant="h2">{Riddles.question}</Typography>
+        <Typography sx={{ mx: 5 }} variant="h2">
+          {riddle.question}
+        </Typography>
       </Box>
-      <Box
-        sx={{
-          mt: 10,
-          ml: 2,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          alignSelf: "center",
-        }}
-      >
-        {Riddles.answerChoices.map((answer, index) => (
-          <Button
-            size="large"
-            variant="contained"
-            key={index}
-            sx={{ mt: 1, ml: 2 }}
-            onClick={checkAnswer.bind(this, answer)}
-          >
-            {answer}
-          </Button>
-        ))}
-      </Box>
+
+      {riddleRows.map((row) => (
+        <Box
+          sx={{
+            mt: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          {row.map((answer) => (
+            <ButtonBase
+              sx={{ mt: 1, mx: 4, width: "100%" }}
+              onClick={checkAnswer.bind(this, answer)}
+            >
+              <Box sx={answerChoiceStyles}>
+                <Typography variant="h4">{answer}</Typography>
+              </Box>
+            </ButtonBase>
+          ))}
+        </Box>
+      ))}
     </>
   );
 };
