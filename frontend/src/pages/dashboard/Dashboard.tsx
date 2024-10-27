e6eb5f5611973eed91d5250357962d9bdba4e734
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-27 06:40:41 -0500

    set: positioning

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Box from "@mui/material/Box/Box";
-import Chart from "./AccFreqChart";
-import ChartUserByAcc from "./PieChart";
 import { useAuth0 } from "@auth0/auth0-react";
 import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
 import GoLogin from "../users/GoLogin";
+import AccPieChart from "./AccPieChart";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import AccFreqGraph from "./AccFreqChart";
+import FreqPieChart from "./FreqPieChart";
+import AccFreqRelationChart from "./AccFreqRelationChart";
 
 const Dashboard = () => {
   const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
@@ -16,17 +19,47 @@ const Dashboard = () => {
     );
   }
 
-  return isAuthenticated && user ? (
+  const forNow = (): boolean => {
+    return true;
+  };
+  return forNow ? (
+    // return isAuthenticated && user ? (
+
     <Box>
       <Box
         sx={{
+          mt: 2,
           display: "flex",
+          gap: 4,
+          px: 4,
+          py: 2,
         }}
       >
-        <ChartUserByAcc />
-        <Chart />
+        <AccPieChart />
+        <AccFreqGraph />
       </Box>
-      <div>Dashboard</div>;
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: 4,
+          px: 4,
+          py: 2,
+        }}
+      >
+        <FreqPieChart />
+        <AccFreqRelationChart />
+      </Box>
+      <Box
+        color="white"
+        position="fixed"
+        bottom={30}
+        width="100%"
+        textAlign="center"
+      >
+        <CopyrightIcon fontSize="small" sx={{ mr: 1 }} />
+        SegFault
+      </Box>
     </Box>
   ) : (
     <GoLogin />
