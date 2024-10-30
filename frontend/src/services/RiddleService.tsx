6a03af71e157f698e02bae028c06d4e8badf78aa
Noe Trevino <noetrevino.music@gmail.com>
2024-10-29 22:37:01 -0500

    change: service into anon func, performant

--- frontend/src/services/RiddleService.tsx
+++ frontend/src/services/RiddleService.tsx
@@ -1,9 +1,10 @@
 import { riddleProps } from "../models/RiddleDTO";
 import RiddlesRepo from "../repository/RiddlesRepository";
-
-export function getRiddle(): riddleProps {
+const getRiddle = (): riddleProps => {
   const randRiddle =
     RiddlesRepo[Math.floor(Math.random() * RiddlesRepo.length)];
 
   return randRiddle;
-}
+};
+
+export default getRiddle;
