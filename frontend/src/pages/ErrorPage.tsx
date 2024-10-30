566110e11b93733d516ebba653f05f5205181767
Noe Trevino <noetrevino.music@gmail.com>
2024-10-29 22:37:11 -0500

    style: error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,18 +1,36 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <>
-      <Card>
-        <Typography variant="h3" mt={5} textAlign={"center"}>
-          Something went wrong...
-        </Typography>
-        <Typography variant="h6" mt={3} textAlign="center">
-          <Link to="/">Click here to go back to the home page</Link>
-        </Typography>
+    <Box
+      justifyContent={"center"}
+      alignContent={"center"}
+      width="100vw"
+      height="100vh"
+    >
+      <Card
+        sx={{
+          backgroundColor: "white",
+          mx: "40rem",
+          opacity: 0.7,
+        }}
+      >
+        <Box justifyContent={"center"}>
+          <Typography
+            variant="h3"
+            mt={5}
+            textAlign={"center"}
+            sx={{ opacity: 1 }}
+          >
+            Something spooky happened...
+          </Typography>
+          <Typography variant="h6" my={3} textAlign="center">
+            <Link to="/">Click here to go back to the home page</Link>
+          </Typography>
+        </Box>
       </Card>
-    </>
+    </Box>
   );
 };
 
